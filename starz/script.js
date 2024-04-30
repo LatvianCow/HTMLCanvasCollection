e2ba35a471c213c39a63041c283bbd8e645ff9f3
ValieX <139682761+Viesf@users.noreply.github.com>
2024-04-30 16:51:14 +0300

    comments

--- starz/script.js
+++ starz/script.js
@@ -14,21 +14,30 @@ for(let key in customization) {
     document.getElementById(key).value = customization[key]
 }
 
+
+// Updates the element values when they're modified in the page by the user
 function update() {
     for(let key in customization) {
         customization[key] = Number(document.getElementById(key).value)
     }
     minGap = customization.minGapMult * customization.size
-    document.body.style.setProperty('--hue', customization.hue)
+    document.body.style.setProperty('--hue', customization.hue) // Adjusts documents --hue variable which is responsible for border colors
 }
 
-const canvas = document.getElementById('kanvas')
-let hue = Number(getComputedStyle(document.body).getPropertyValue('--hue'))
 
+const canvas = document.getElementById('kanvas')
+let hue = Number(getComputedStyle(document.body).getPropertyValue('--hue')) // Gets the hue value set in the style.css file
+
+
+// Adjusts canva's width and height paramteres when page is resized
 let CWidth, CHeight
 function onResize() {
+    // Gets style for the element containing the background for the canvas and the canvas
     let bStyle = getComputedStyle(canvas.parentElement.parentElement)
+
+    // Calculates the new size for the canvas
     let bodyWidth = bStyle.width.slice(0, -2) - bStyle.paddingLeft.slice(0, -2) - bStyle.paddingRight.slice(0, -2)
+    
     let ARatio = 2 / 3
 
     canvas.setAttribute('width', bodyWidth)
@@ -38,15 +47,20 @@ function onResize() {
     CHeight = bodyWidth * ARatio
 } onResize(); addEventListener('resize', onResize)
 
+
 /** @type {CanvasRenderingContext2D} */
 const ctx = canvas.getContext('2d')
 
 let stars = []
 
+
+// function to check wether given position is not too close to any of the stars
 function legalPos(pos) {
     return !stars.some((star) => (Math.abs(star.x - pos.x) < minGap * star.dist && Math.abs(star.y - pos.y) < minGap * star.dist))
 }
 
+
+// Generates new array of stars
 function gen() {
     stars = []
     for(let i = 0; i < customization.maxCount; i++) {
@@ -62,28 +76,32 @@ function gen() {
     draw()
 } gen()
 
+
+// Calculates the size alter for star
 function sizeAlt(x) {
     return 1 - 0.1 / (0.1 + x) - (0.9091 / 1) * Math.pow(Math.cos(1.5 * (x - 1)), 23)
 }
 
+
+// Draws all of the stars
 function draw() {
     ctx.clearRect(0, 0, CWidth, CHeight)
 
+    ctx.fillStyle = `rgba(255, 255, 255, 1)`
+    ctx.strokeStyle = `hsl(${customization.hue}, 100%, 50%)`
     stars.forEach((star) => {   
-        ctx.fillStyle = `rgba(255, 255, 255, 1)`
-        ctx.strokeStyle = `hsl(${customization.hue}, 100%, 50%)`
         ctx.lineWidth = customization.strokeSize * star.dist
         
+        // Calculates the size rotation and control points for star
         let rotation = (star.x * 31 + star.y * 17 + rotationAlt) % 90
         let Asize = customization.size * star.dist - sizeAlt(rotation % 90 / 90) * 5
         let controlPoint = customization.controlPoint * star.dist
         
+        // Translates the star over to its position and rotates it
         ctx.translate(star.x, star.y)
         ctx.rotate(Math.PI / 180 * rotation)
 
-        // ctx.fillRect(-Asize / 2, -Asize / 2, Asize, Asize)
-        // ctx.strokeRect(-Asize / 2, -Asize / 2, Asize, Asize)
-
+        // Draws the star
         ctx.beginPath()
 
         ctx.moveTo(Asize / 2, 0)
@@ -98,11 +116,14 @@ function draw() {
         ctx.fill()
         ctx.stroke()
         
+        // Untranslates and unrotates the canvas
         ctx.rotate(-Math.PI / 180 * rotation)
         ctx.translate(-star.x, -star.y)
     })
 }
 
+
+// Star rotater
 setInterval(() => {
 
     rotationAlt = (rotationAlt + customization.rotIncr) % 90
